9f5e9b916d59d1a63d0b52eb935c3ae5d5355098
Mellon <JackMeiLong@users.noreply.github.com>
2016-08-02 22:48:34 +0800

    Update TalkingData.py

--- TalkingData_Mobile_User_Demographics/TalkingData.py
+++ TalkingData_Mobile_User_Demographics/TalkingData.py
@@ -1,17 +1,21 @@
-# coding=utf8
+# -*- coding: utf-8 -*-
 """
-Created on Tue Jul 12 11:53:08 2016
+Created on Tue Aug 02 19:30:51 2016
 
 @author: meil
 """
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import OneHotEncoder
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn.feature_extraction import FeatureHasher
 from sklearn.preprocessing import LabelEncoder
 
-events = pd.read_csv("events.csv", dtype = {"device_id": np.str}, 
-                     infer_datetime_format = True, parse_dates = ["timestamp"])
+from sklearn.ensemble import RandomForestClassifier
+
+# Any results you write to the current directory are saved as output.
+
+# loading data
+events = pd.read_csv("events.csv", dtype = {"device_id": np.str}, infer_datetime_format = True, parse_dates = ["timestamp"])
 app_events = pd.read_csv("app_events.csv", usecols = [0, 2, 3],
                             dtype = {"is_active": np.float16, "is_installed": np.float16})
 
@@ -25,35 +29,21 @@ events = events.merge(app_events, how = "left", on = "event_id")
 events.drop("event_id", axis = 1, inplace = True)
 
 # prep brands
-phone = pd.read_csv("phone_brand_device_model.csv", dtype={"device_id": np.str},
-                    usecols = [0, 1, 2])
+phone = pd.read_csv("phone_brand_device_model.csv", dtype={"device_id": np.str})
 
-phone_brand=pd.concat([phone['phone_brand']],axis=1)
-device_model=pd.concat([phone['device_model']],axis=1)
+# feature hasher
+feat = FeatureHasher(n_features=12, input_type="string", dtype=np.float32)
+#print(feat)
 
-le=LabelEncoder()
-pb_label=le.fit_transform(phone_brand)
-dm_label=le.fit_transform(device_model)
+feat1 = feat.transform(phone["phone_brand"] + " " + phone["device_model"])
 
-ph_list=[]
-ph_dict=[]
+#print(feat1) 
 
-for i in range(pb_label.shape[0]):
-    ph_list.append([pb_label[i],dm_label[i]])
-    ph_dict.append({phone_brand.columns[0]:pb_label[i],device_model.columns[0]:dm_label[i]})
-    
-enc = OneHotEncoder()
+events = events.merge(pd.concat([phone["device_id"], pd.DataFrame(feat1.toarray())], axis = 1), how = "left", on = "device_id")
 
-ph_encode=enc.fit_transform(ph_list).toarray()
+print(events.head(5))
 
-ph_df=pd.DataFrame(ph_encode)
-
-feat=FeatureHasher(n_features=10)
-ph_fe=feat.fit_transform(ph_dict).toarray()
-phfe_df=pd.DataFrame(ph_fe)
-
-events = events.merge(pd.concat([phone["device_id"],phfe_df], axis = 1),
-                     how = "left", on = "device_id")
+#del phone, feat, feat1
 
 print("pre-merging and hashing finished.")
 
@@ -64,30 +54,54 @@ t2 = train.copy()
 train.drop("group", axis = 1, inplace = True)
 train = train.merge(events, how = "left", on = "device_id")
 train.fillna(-1, inplace = True)
-
-#tt=pd.concat([train.phone_brand,phone.device_model],axis=1)
-#phone_brand=train.phone_brand
-#device_model=train.device_model
-
 train = train.groupby("device_id").mean().reset_index()
 train = train.merge(t2, how ="left", on = "device_id")
 
+le=LabelEncoder()
+
 label = train["group"].copy()
-#le=LabelEncoder()
-lable=le.fit_transform(label)
+clas=le.fit_transform(label)
 
 train.drop(["group", "device_id"], axis = 1, inplace = True)
+del t2
 
 print("train data merged and prepared")
 print("-----------------------------------")
+print(train.info())
+print(train.head(5))
+print("-----------------------------------")
 
-print 'Starting to prepare testdata'
-test=pd.read_csv('gender_age_test.csv',dtype={'device_id':np.str})
+# load test data merge with events
+test = pd.read_csv("gender_age_test.csv", dtype = {"device_id": np.str})
 
-test=test.merge(events,how='left',on='device_id')
-test.fillna(-1,inplace=True)
+test = test.merge(events, how = "left", on = "device_id")
+#del events
+print("test loaded and merged")
 
-test=test.groupby('device_id').mean().reset_index()
-#test.drop(['device_id'],axis=1,inplace=True)
-print 'test data prepared'
+test.fillna(-1, inplace = True)
+test["hour"] = test["hour"].astype(np.float16)
+test = test.groupby("device_id").mean().reset_index()
+ids = test["device_id"].copy()
+test.drop("device_id", axis = 1, inplace = True)
 
+print("test prepared")
+print("-----------------------------------")
+print(test.info())
+print("-----------------------------------")
+
+#Train
+
+rfc=RandomForestClassifier(n_estimators=10, criterion='gini')
+X=train.values
+y=clas
+rfc.fit(X,y)
+
+y_pred=rfc.predict_proba(test)
+
+
+sub =pd.concat([ids,pd.DataFrame(y_pred)],axis=1)
+#del pred
+print("shape of submission: " + str(sub.shape))
+sub.to_csv("sample_submission.csv", index = False)
+#del sub
+print("submission saving finished.")
